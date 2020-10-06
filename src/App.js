6f1b6f8d58a325489d574e963c956055a054f759
Aidan McBride <aidankmcbride@gmail.com>
2020-10-05 21:20:55 -0400

    netlify deployed

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <h1>Email Form</h1>
-      <form name="contact" metehod="post" data-netlify="true">
+      <form name="contact" method="post" data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
         <label className="label">Name</label><input required className="input" type="text" placeholder="First Last" name="name"/><br/>
         <label className="label">Email</label><input required className="input" type="email" placeholder="ex@example.com" name="email"/><br/>
